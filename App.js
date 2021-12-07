6cb39b30f694bfdfe3426fd9f6ddad16f8ce116f
Edwin Alfaro <edwinalfaro9908@gmail.com>
2021-12-06 20:33:21 -0500

    tab navigation

--- App.js
+++ App.js
@@ -9,6 +9,7 @@ import LandingScreen from './components/auth/Landing';
 import RegisterScreen from './components/auth/Register';
 import LoginScreen from './components/auth/Login';
 import MainScreen from './components/Main';
+import AddScreen from './components/main/Add';
 
 import store from './redux/store.js';
 
@@ -52,7 +53,16 @@ const App = () => {
   }
   return (
     <Provider store={store}>
-      <MainScreen />
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Main">
+          <Stack.Screen
+            name="Main"
+            component={MainScreen}
+            options={{headerShown: false}}
+          />
+          <Stack.Screen name="Add" component={AddScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
     </Provider>
   );
 };
--- components/Main.js
+++ components/Main.js
@@ -1,33 +1,85 @@
 import React, {useEffect} from 'react';
-import {View, Text, Button} from 'react-native';
-import auth from '@react-native-firebase/auth';
-import {useDispatch, useSelector} from 'react-redux';
+import {useDispatch} from 'react-redux';
 import {fetchUser} from '../redux/actions';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+
+import FeedScreen from './main/Feed';
+import ProfileScreen from './main/Profile';
+
+const Tab = createBottomTabNavigator();
+
+const EmptyScreen = () => {
+  return null;
+};
 
 const Main = () => {
   const dispatch = useDispatch();
-  const currentUser = useSelector(state => state.user.currentUser);
 
   useEffect(() => {
     dispatch(fetchUser());
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  if (!currentUser) {
-    return <View></View>;
-  }
   return (
-    <View style={{flex: 1, justifyContent: 'center'}}>
-      <Text>{currentUser.name} is logged in</Text>
-      <Button
-        title="Sign out"
-        onPress={() =>
-          auth()
-            .signOut()
-            .then(() => console.log('User signed out'))
-        }
+    <Tab.Navigator initialRouteName="Feed">
+      <Tab.Screen
+        name="Feed"
+        component={FeedScreen}
+        options={{
+          tabBarIcon: ({color, size}) => {
+            return (
+              <MaterialCommunityIcons name="home" color={color} size={26} />
+            );
+          },
+        }}
       />
-    </View>
+      <Tab.Screen
+        name="AddContainer"
+        listeners={({navigation}) => {
+          return {
+            tabPress: e => {
+              e.preventDefault();
+              navigation.navigate('Add');
+            },
+          };
+        }}
+        component={EmptyScreen}
+        options={{
+          tabBarIcon: ({color, size}) => {
+            return (
+              <MaterialCommunityIcons name="plus-box" color={color} size={26} />
+            );
+          },
+        }}
+      />
+      <Tab.Screen
+        name="Profile"
+        component={ProfileScreen}
+        options={{
+          tabBarIcon: ({color, size}) => {
+            return (
+              <MaterialCommunityIcons
+                name="account-circle"
+                color={color}
+                size={26}
+              />
+            );
+          },
+        }}
+      />
+    </Tab.Navigator>
+    // <View style={{flex: 1, justifyContent: 'center'}}>
+    //   <Text>{currentUser.name} is logged in</Text>
+    //   <Button
+    //     title="Sign out"
+    //     onPress={() =>
+    //       auth()
+    //         .signOut()
+    //         .then(() => console.log('User signed out'))
+    //     }
+    //   />
+    // </View>
   );
 };
 
